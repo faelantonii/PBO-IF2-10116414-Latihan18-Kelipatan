0d7ec6dcbf720d7bbc310de3736f23ed758e7c3a
sweenutt <sweenutt@192.168.3.19>
2020-10-31 21:50:23 +0700

    Latihan 17 Program Kelipatan

---
+++ src/PBO_IF2_10116414_Latihan18_Kelipatan/PBO_IF2_10116414_Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_IF2_10116414_Latihan18_Kelipatan;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * DESKRIPSI    : Program Kelipatan Angka
+ */
+public class PBO_IF2_10116414_Kelipatan {
+    public static void main(String[] args) {
+        float bil;
+        for(bil=(float) 3.5;bil<=35;bil+=3.5){
+            {
+                System.out.println(bil);
+            }
+        }
+    }
+}
+
+
